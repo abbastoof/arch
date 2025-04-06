49c543f312d2ac45b6e3c7337ee5e9fc64a95d79
Abbas(Pooria) Toof <abbas.toof@gmail.com>
2025-04-06 23:06:59 +0300

    Update btrfs.sh

--- btrfs.sh
+++ btrfs.sh
@@ -1,36 +1,51 @@
 #!/bin/bash
 set -e
 
-# Format the root partition as Btrfs
-mkfs.btrfs -f /dev/nvme0n1p2
+# Variables (change these if needed)
+ROOT_PART="/dev/nvme0n1p4"
+EFI_PART="/dev/nvme0n1p1"
+MOUNTPOINT="/mnt"
+BTRFS_OPTS="noatime,compress=zstd,commit=120,space_cache=v2"
 
-# Create subvolumes
-mount /dev/nvme0n1p2 /mnt
-btrfs subvolume create /mnt/@
-btrfs subvolume create /mnt/@home
-btrfs subvolume create /mnt/@var
-btrfs subvolume create /mnt/@log
-btrfs subvolume create /mnt/@tmp
-btrfs subvolume create /mnt/@pkg
-btrfs subvolume create /mnt/@snapshots
-umount /mnt
+# WARNING: This will erase all data on ${ROOT_PART}
+echo "Formatting ${ROOT_PART} as Btrfs..."
+mkfs.btrfs -f "$ROOT_PART"
 
-# Mount the root partition with subvolume @ and specific options
-mount -o noatime,compress=zstd,subvol=@,commit=120,space_cache=v2 /dev/nvme0n1p2 /mnt
+# Mount the partition temporarily to create subvolumes
+echo "Mounting ${ROOT_PART} temporarily to create subvolumes..."
+mount "$ROOT_PART" "$MOUNTPOINT"
 
-# Create mount points for subvolumes (if they don't exist)
-mkdir -p /mnt/home /mnt/var /mnt/.snapshots /mnt/boot/efi
+echo "Creating Btrfs subvolumes..."
+btrfs subvolume create "$MOUNTPOINT/@"
+btrfs subvolume create "$MOUNTPOINT/@home"
+btrfs subvolume create "$MOUNTPOINT/@var"
+btrfs subvolume create "$MOUNTPOINT/@log"
+btrfs subvolume create "$MOUNTPOINT/@tmp"
+btrfs subvolume create "$MOUNTPOINT/@pkg"
+btrfs subvolume create "$MOUNTPOINT/@snapshots"
+
+# Unmount the temporary mount
+umount "$MOUNTPOINT"
+
+# Mount the subvolumes using our desired options
+echo "Mounting the Btrfs subvolumes..."
+
+# Mount root subvolume
+mount -o $BTRFS_OPTS,subvol=@ "$ROOT_PART" "$MOUNTPOINT"
+
+# Create all needed directories BEFORE mounting nested subvolumes
+mkdir -p "$MOUNTPOINT"/{home,var,var/log,var/tmp,var/cache/pacman/pkg,.snapshots,boot}
 
 # Mount each subvolume
-mount -o noatime,compress=zstd,subvol=@home,commit=120,space_cache=v2 /dev/nvme0n1p2 /mnt/home
-mount -o noatime,compress=zstd,subvol=@var,commit=120,space_cache=v2 /dev/nvme0n1p2 /mnt/var
-mount -o noatime,compress=zstd,subvol=@log,commit=120,space_cache=v2 /dev/nvme0n1p2 /mnt/var/log
-mount -o noatime,compress=zstd,subvol=@tmp,commit=120,space_cache=v2 /dev/nvme0n1p2 /mnt/var/tmp
-mount -o noatime,compress=zstd,subvol=@pkg,commit=120,space_cache=v2 /dev/nvme0n1p2 /mnt/var/cache/pacman/pkg
-mount -o noatime,compress=zstd,subvol=@snapshots,commit=120,space_cache=v2 /dev/nvme0n1p2 /mnt/.snapshots
+mount -o $BTRFS_OPTS,subvol=@home "$ROOT_PART" "$MOUNTPOINT/home"
+mount -o $BTRFS_OPTS,subvol=@var "$ROOT_PART" "$MOUNTPOINT/var"
+mount -o $BTRFS_OPTS,subvol=@log "$ROOT_PART" "$MOUNTPOINT/var/log"
+mount -o $BTRFS_OPTS,subvol=@tmp "$ROOT_PART" "$MOUNTPOINT/var/tmp"
+mount -o $BTRFS_OPTS,subvol=@pkg "$ROOT_PART" "$MOUNTPOINT/var/cache/pacman/pkg"
+mount -o $BTRFS_OPTS,subvol=@snapshots "$ROOT_PART" "$MOUNTPOINT/.snapshots"
 
-# Create directories inside /mnt/var subvolume after mounting it
-mkdir -p /mnt/var/log /mnt/var/tmp /mnt/var/cache/pacman/pkg
+# Mount the EFI partition (shared EFI)
+mount "$EFI_PART" "$MOUNTPOINT/boot"
 
-# Mount EFI partition
-mount /dev/nvme0n1p1 /mnt/boot/efi
+echo "All done. Your subvolumes are mounted as follows:"
+mount | grep "$ROOT_PART"
