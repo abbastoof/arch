f10b2c51ae0cf75896d9bc3737f3bfda906c0245
Abbas(Pooria) Toof <abbas.toof@gmail.com>
2025-04-06 22:34:06 +0300

    Update btrfs.sh

--- btrfs.sh
+++ btrfs.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-# Exit immediately if a command exits with a non-zero status
 set -e
 
 # Format the root partition as Btrfs
 mkfs.btrfs -f /dev/nvme0n1p2
 
-# Create subvolumes on the partition
+# Create subvolumes
 mount /dev/nvme0n1p2 /mnt
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
@@ -19,10 +18,10 @@ umount /mnt
 # Mount the root partition with subvolume @ and specific options
 mount -o noatime,compress=zstd,subvol=@,commit=120,space_cache=v2 /dev/nvme0n1p2 /mnt
 
-# Create directories for the other subvolumes
-mkdir -p /mnt/{home,var,var/log,var/tmp,var/cache/pacman/pkg,.snapshots}
+# Create mount points for subvolumes (if they don't exist)
+mkdir -p /mnt/home /mnt/var /mnt/.snapshots /mnt/boot/efi
 
-# Mount each subvolume with desired options
+# Mount each subvolume
 mount -o noatime,compress=zstd,subvol=@home,commit=120,space_cache=v2 /dev/nvme0n1p2 /mnt/home
 mount -o noatime,compress=zstd,subvol=@var,commit=120,space_cache=v2 /dev/nvme0n1p2 /mnt/var
 mount -o noatime,compress=zstd,subvol=@log,commit=120,space_cache=v2 /dev/nvme0n1p2 /mnt/var/log
@@ -30,6 +29,8 @@ mount -o noatime,compress=zstd,subvol=@tmp,commit=120,space_cache=v2 /dev/nvme0n
 mount -o noatime,compress=zstd,subvol=@pkg,commit=120,space_cache=v2 /dev/nvme0n1p2 /mnt/var/cache/pacman/pkg
 mount -o noatime,compress=zstd,subvol=@snapshots,commit=120,space_cache=v2 /dev/nvme0n1p2 /mnt/.snapshots
 
+# Create directories inside /mnt/var subvolume after mounting it
+mkdir -p /mnt/var/log /mnt/var/tmp /mnt/var/cache/pacman/pkg
+
 # Mount EFI partition
-mkdir -p /mnt/boot/efi
 mount /dev/nvme0n1p1 /mnt/boot/efi
