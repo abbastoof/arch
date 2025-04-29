dfc7101b0609ee6781ec3adc906da73ab89dd2f9
Abbas(Pooria) Toof <abbas.toof@gmail.com>
2025-04-29 09:40:48 +0300

    Update grub.sh

--- grub.sh
+++ grub.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-
-# Exit immediately if any command fails
 set -e
 
-echo "Installing grub, efibootmgr, os-prober..."
-pacman -S --noconfirm grub efibootmgr os-prober
+# Variables
+EFI_DIR="/boot/efi"
+BOOTLOADER_ID="arch"
 
-echo "Enabling os-prober detection in GRUB..."
-if ! grep -q "GRUB_DISABLE_OS_PROBER=false" /etc/default/grub; then
+echo "==> Installing required packages (grub, efibootmgr, os-prober)..."
+pacman -Sy --noconfirm grub efibootmgr os-prober
+
+echo "==> Enabling os-prober in GRUB configuration..."
+if ! grep -q 'GRUB_DISABLE_OS_PROBER=false' /etc/default/grub; then
     echo 'GRUB_DISABLE_OS_PROBER=false' >> /etc/default/grub
 fi
 
-echo "Making sure /boot/grub directory exists..."
+echo "==> Creating /boot/grub directory if it doesn't exist..."
 mkdir -p /boot/grub
 
-echo "Installing GRUB to EFI partition..."
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch --recheck
+echo "==> Installing GRUB to EFI partition..."
+grub-install --target=x86_64-efi --efi-directory="$EFI_DIR" --bootloader-id="$BOOTLOADER_ID" --recheck
 
-echo "Generating GRUB configuration..."
+echo "==> Generating grub.cfg..."
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "Done! GRUB is installed and configured with OS detection."
+echo "==> Done! GRUB setup complete. Multiboot ready."
