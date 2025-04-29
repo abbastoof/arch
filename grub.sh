56349c45d5d7892ff3978ab4e88a7bd8e97f8fc2
Abbas(Pooria) Toof <abbas.toof@gmail.com>
2025-04-29 09:56:39 +0300

    Update grub.sh

--- grub.sh
+++ grub.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 set -e
 
-# Variables
-EFI_DIR="/boot/efi"
-BOOTLOADER_ID="arch"
-
-echo "==> Installing required packages (grub, efibootmgr, os-prober)..."
+echo "==> Installing GRUB, efibootmgr, os-prober..."
 pacman -Sy --noconfirm grub efibootmgr os-prober
 
-echo "==> Enabling os-prober in GRUB configuration..."
-if ! grep -q 'GRUB_DISABLE_OS_PROBER=false' /etc/default/grub; then
-    echo 'GRUB_DISABLE_OS_PROBER=false' >> /etc/default/grub
+# Make sure /boot/grub exists
+if [ ! -d /boot/grub ]; then
+    echo "==> Creating /boot/grub..."
+    mkdir -p /boot/grub
 fi
 
-echo "==> Creating /boot/grub directory if it doesn't exist..."
-mkdir -p /boot/grub
+# Clean up invalid /boot/efi/grub if present
+if [ -d /boot/efi/grub ]; then
+    echo "==> Removing unnecessary /boot/efi/grub..."
+    rm -rf /boot/efi/grub
+fi
 
-echo "==> Installing GRUB to EFI partition..."
-grub-install --target=x86_64-efi --efi-directory="$EFI_DIR" --bootloader-id="$BOOTLOADER_ID" --recheck
+# Enable os-prober support in GRUB
+echo "==> Enabling os-prober support..."
+GRUB_CONFIG="/etc/default/grub"
+if grep -q "^#?GRUB_DISABLE_OS_PROBER=.*" "$GRUB_CONFIG"; then
+    sudo sed -i 's/^#\?GRUB_DISABLE_OS_PROBER=.*/GRUB_DISABLE_OS_PROBER=false/' "$GRUB_CONFIG"
+else
+    echo "GRUB_DISABLE_OS_PROBER=false" >> "$GRUB_CONFIG"
+fi
 
-echo "==> Generating grub.cfg..."
+# Reinstall GRUB to the EFI partition
+echo "==> Installing GRUB bootloader to /boot/efi/EFI/Arch..."
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch --recheck
+
+# Generate GRUB config with OS detection
+echo "==> Generating GRUB config..."
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "==> Done! GRUB setup complete. Multiboot ready."
+echo "==> Done! GRUB is installed, os-prober enabled, and config generated."
